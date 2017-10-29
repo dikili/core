c3eed4e1a11e5f9a346d5e58dd7bbb5c77540d08
dikili <dikili@outlook.com>
2017-10-29 21:03:13 +0000

    repositories that we will use

---
+++ CoreApplication.Data/DataEntities/MongoEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreApplication.Data.DataEntities.Interfaces;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CoreApplication.Data.DataEntities
+{
+    /// <summary>
+    /// Abstract Mongo Entity.
+    /// </summary>
+    /// <remarks>Mongo Entities are assumed to use strings for Id's.</remarks>
+    public abstract class MongoEntity : IEntity<string>
+    {
+        /// <summary>
+        /// Initializes a new instance of the MongoEntity class.
+        /// </summary>
+        protected MongoEntity()
+        {
+            Id = ObjectId.GenerateNewId().ToString();
+        }
+
+        /// <summary>
+        /// Gets or sets the id for this object (the primary record for an entity).
+        /// </summary>
+        /// <value>The id for this object (the primary record for an entity).</value>
+        [BsonId]
+        public string Id { get; set; }
+    }
+}
---
+++ CoreApplication.Data/Settings/MongoSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreApplication.Data.Settings
+{
+    public class MongoSettings
+    {
+        public string User { get; set; }
+        public string Pass { get; set; }
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public string Data { get; set; }
+    }
+}
