955cacb0f172905912141a53d076521b53c15927
ICENI\id1 <dikili@outlook.com>
2017-10-30 10:43:35 +0000

    push to be able to get a pull

---
+++ CoreApplication.Data/DataEntities/Interfaces/IEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreApplication.Data.DataEntities.Interfaces
+{
+    /// <summary>
+
+    /// Generic Entity interface.
+
+    /// </summary>
+
+    /// <typeparam name="TKey">The type used for the entity's Id.</typeparam>
+
+    public interface IEntity<TKey>
+
+    {
+
+        /// <summary>
+
+        /// Gets or sets the Id of the Entity.
+
+        /// </summary>
+
+        /// <value>Id of the Entity.</value>
+
+        TKey Id { get; set; }
+
+    }
+}
