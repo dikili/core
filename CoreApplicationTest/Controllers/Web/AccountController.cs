2814d2a9109f383babfbe92342384a7b3c285dc0
dikili <dikili@outlook.com>
2017-12-15 20:33:55 +0000

    added a login page and related action for it...

---
+++ CoreApplicationTest/Controllers/Web/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CoreApplicationTest.Controllers.Web
+{
+    public class AccountController :Controller
+    {
+        private ILogger<AccountController> _logger;
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "App");
+            }
+
+            return View();
+        }
+
+    }
+}
