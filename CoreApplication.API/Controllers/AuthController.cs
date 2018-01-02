e824f6b7f7b69eb24620e4d3b700d80072b11164
dikili <dikili@outlook.com>
2018-01-02 19:52:06 +0000

    login function implementation,token generation jwt

--- CoreApplication.API/Controllers/AuthController.cs
+++ CoreApplication.API/Controllers/AuthController.cs
@@ -1,8 +1,13 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using CoreApplication.API.DTOs;
 using CoreApplication.Data.DataEntities;
 using CoreApplication.Data.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CoreApplication.API.Controllers
 {
@@ -39,6 +44,35 @@ namespace CoreApplication.API.Controllers
            // return CreatedAtRoute()
 
            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto user)
+        {
+             var userFromRepo=await _repo.Login(user.UserName,user.Password);
+
+             if(userFromRepo==null)
+               return Unauthorized();
+
+             var tokenHandler=new JwtSecurityTokenHandler(); 
+
+             var key=Encoding.ASCII.GetBytes("super secret key");
+
+             var tokenDescriptor=new SecurityTokenDescriptor{
+                 Subject= new ClaimsIdentity(new Claim[]{
+                     new Claim(ClaimTypes.NameIdentifier,userFromRepo.Id.ToString()),
+                     new Claim(ClaimTypes.Name,userFromRepo.UserName)
+                 }),
+                 Expires=DateTime.Now.AddDays(1),
+                 SigningCredentials=new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha512Signature)
+             };
+
+             var token=tokenHandler.CreateToken(tokenDescriptor);
+
+             var tokenString=tokenHandler.WriteToken(token);
+
+             return Ok(new {tokenString}); 
+         
         }
 
     }
