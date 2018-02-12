a37dcbce4f62fede5bbe2f90261bc66638c710ad
dikili <dikili@outlook.com>
2018-02-12 20:22:14 +0000

    member edit component initial creation and its resolver

---
+++ CoreApplicationTest.UI/src/app/_resolvers/member-edit.resolver.ts
@@ -0,0 +1,31 @@
+import { Resolve } from '@angular/router';
+import { User } from '../_models/User';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { ActivatedRouteSnapshot } from '@angular/router/src/router_state';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
+import { Injectable } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+
+@Injectable()
+export class MemberEditResolver implements Resolve<User> {
+
+/**
+ *
+ */
+constructor(private userService: UserService,  private alertify: AlertifyService , private authService: AuthService) {
+
+}
+
+resolve(route: ActivatedRouteSnapshot): Observable<User>  {
+  return this.userService.getUser(this.authService.decodedToken.nameid)
+        .catch(error => {
+      this.alertify.error('Problem retrieving data');
+      return Observable.of(null);
+  });
+}
+}
--- CoreApplicationTest.UI/src/app/app.module.ts
+++ CoreApplicationTest.UI/src/app/app.module.ts
@@ -25,6 +25,8 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 
 
 
@@ -42,7 +44,8 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
     ListsComponent,
     MessagesComponent,
     MemberCardComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberEditComponent
 ],
   imports: [
     BrowserModule,
@@ -59,7 +62,8 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
      AuthGuard,
      UserService,
      MemberDetailResolver,
-     MemberListResolver
+     MemberListResolver,
+     MemberEditResolver
     ],
   bootstrap: [AppComponent]
 })
