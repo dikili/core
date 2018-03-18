04a23ed1182f9192eefa3049dd861a670f58cbce
dikili <dikili@outlook.com>
2018-03-18 15:36:14 +0000

    photo editor component which is a child component of memb-edit comp.imp.

--- CoreApplicationTest.UI/src/app/app.module.ts
+++ CoreApplicationTest.UI/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 
 
 
@@ -46,7 +47,8 @@ import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
     MessagesComponent,
     MemberCardComponent,
     MemberDetailComponent,
-    MemberEditComponent
+    MemberEditComponent,
+    PhotoEditorComponent
 ],
   imports: [
     BrowserModule,
--- CoreApplicationTest/Controllers/Web/AccountController.cs
+++ CoreApplicationTest/Controllers/Web/AccountController.cs
@@ -43,46 +43,46 @@ namespace CoreApplicationTest.Controllers.Web
             return View();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                //var result =  await _signInManager.PasswordSignInAsync(model.UserName,
-                //     model.Password,
-                //     model.RememberMe,
-                //     false);
+        // [HttpPost]
+        // public async Task<IActionResult> Login(LoginViewModel model)
+        // {
+        //     if (ModelState.IsValid)
+        //     {
+        //         //var result =  await _signInManager.PasswordSignInAsync(model.UserName,
+        //         //     model.Password,
+        //         //     model.RememberMe,
+        //         //     false);
 
-                // if (result.Succeeded)
-                // {
-                //     // When Authorize attribute is used returnurl is appended as we saw
-                //     // If that happens we can say that redirect to returnUrl if it exist if not go to the default as in else below
-                //     if (Request.Query.Keys.Contains("ReturnUrl"))
-                //     {
-                //        return Redirect(Request.Query["ReturnUrl"].First());
-                //     }
-                //     //else
-                //     //{
-                //        return RedirectToAction("Index", "App");
-                //    // }
+        //         // if (result.Succeeded)
+        //         // {
+        //         //     // When Authorize attribute is used returnurl is appended as we saw
+        //         //     // If that happens we can say that redirect to returnUrl if it exist if not go to the default as in else below
+        //         //     if (Request.Query.Keys.Contains("ReturnUrl"))
+        //         //     {
+        //         //        return Redirect(Request.Query["ReturnUrl"].First());
+        //         //     }
+        //         //     //else
+        //         //     //{
+        //         //        return RedirectToAction("Index", "App");
+        //         //    // }
                     
-                // }
-            }
+        //         // }
+        //     }
 
-            ModelState.AddModelError("","Failed to Login");
-           // var user = await _signInManager.UserManager.FindByNameAsync(model.UserName);
+        //     ModelState.AddModelError("","Failed to Login");
+        //    // var user = await _signInManager.UserManager.FindByNameAsync(model.UserName);
 
-            //var res= _signInManager.SignInAsync(user, true);
-            //unless the sign in successful we should go to the View
-            return View();
-        }
+        //     //var res= _signInManager.SignInAsync(user, true);
+        //     //unless the sign in successful we should go to the View
+        //     return View();
+        // }
 
-        [HttpGet]
-        public async Task<IActionResult> Logout()
-        {
-           // await _signInManager.SignOutAsync();
-           return RedirectToAction("Index", "App");
-        }
+        // [HttpGet]
+        // public async Task<IActionResult> Logout()
+        // {
+        //    // await _signInManager.SignOutAsync();
+        //    return RedirectToAction("Index", "App");
+        // }
 
         // [HttpPost]
         // public async Task<IActionResult> CreateToken([FromBody] LoginViewModel model)
