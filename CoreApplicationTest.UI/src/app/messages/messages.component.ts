eaf0cfc65c71c488944d95112e132fed7c299fcb
dikili <dikili@outlook.com>
2018-09-03 19:32:09 +0100

    changes for wwwroot mainly

--- CoreApplicationTest.UI/src/app/messages/messages.component.ts
+++ CoreApplicationTest.UI/src/app/messages/messages.component.ts
@@ -33,7 +33,6 @@ this.pagination = data['messages'].pagination;
 
   loadMessages() {
     // tslint:disable-next-line:no-debugger
-    debugger;
     this.userService
     .getMessages(this.authService.decodedToken.nameid,
       this.pagination.currentPage,
