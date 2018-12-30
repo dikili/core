e3598812248e3e5ef9d1a7a4c491ef5cd2d10ed9
dikili <dikili@outlook.com>
2018-12-30 18:39:05 +0000

    new build

--- CoreApplication.API/Startup.cs
+++ CoreApplication.API/Startup.cs
@@ -97,7 +97,11 @@ namespace CoreApplication.API
                    };
                });
 
-
+    services.AddAuthorization(options =>  {
+                options.AddPolicy("RequireAdminRole", policy=>policy.RequireRole("Admin"));
+                options.AddPolicy("ModeratePhotoRole", policy=>policy.RequireRole("Admin","Moderator"));
+                options.AddPolicy("VipOnly", policy=>policy.RequireRole("VIP"));
+              });
 
             services.Configure<CloudinarySettings>(_config.GetSection("CloudinarySettings"));
             
