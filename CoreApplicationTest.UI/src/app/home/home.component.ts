8efa95e2c194e0205fe8c418d37048b9610ae241
dikili <dikili@outlook.com>
2025-12-29 23:09:08 +0000

    latest_now

--- CoreApplicationTest.UI/src/app/home/home.component.ts
+++ CoreApplicationTest.UI/src/app/home/home.component.ts
@@ -1,32 +1,31 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-
+import { Component, OnInit, TemplateRef } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  registerMode= false;
+  registerMode = false;
   values: any;
   modalRef: BsModalRef;
 
-  constructor(private http: HttpClient, private modalService: BsModalService) { }
+  constructor(private http: HttpClient, private modalService: BsModalService) {}
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
   ngOnInit() {
-   // this.getValues();
+    // this.getValues();
   }
 
   registerToggle() {
     this.registerMode = true;
   }
   getValues() {
-    this.http.get('http://localhost:8000/api/values').subscribe(response => {
+    this.http.get("http://localhost:8005/api/values").subscribe((response) => {
       console.log(response);
 
       this.values = response;
@@ -34,7 +33,7 @@ export class HomeComponent implements OnInit {
   }
 
   cancelRegister(cancelFlag: boolean) {
-   this.registerMode = cancelFlag;
-   this.modalRef.hide();
+    this.registerMode = cancelFlag;
+    this.modalRef.hide();
   }
 }
--- CoreApplicationTest.UI/src/app/nav/nav.component.ts
+++ CoreApplicationTest.UI/src/app/nav/nav.component.ts
@@ -1,35 +1,43 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { AuthService } from '../_services/auth.service';
-import { AlertifyService } from '../_services/alertify.service';
-import { Router } from '@angular/router';
-import { UserService } from '../_services/user.service';
-import { stringify } from 'querystring';
-import { Message } from '../_models/message';
-import { Pagination, PaginatedResult } from '../_models/pagination';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { Component, OnInit, TemplateRef } from "@angular/core";
+import { AuthService } from "../_services/auth.service";
+import { AlertifyService } from "../_services/alertify.service";
+import { Router } from "@angular/router";
+import { UserService } from "../_services/user.service";
+import { stringify } from "querystring";
+import { Message } from "../_models/message";
+import { Pagination, PaginatedResult } from "../_models/pagination";
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"],
 })
 export class NavComponent implements OnInit {
   model: any = {};
   photoUrl: string;
   messages: Message[];
   pagination: Pagination;
-  messageContainer: 'Unread';
+  messageContainer: "Unread";
   isUnRead: boolean;
   modalRef: BsModalRef;
-  registerMode= false;
+  registerMode = false;
 
-
-  constructor(public authService: AuthService, private alertify: AlertifyService, private userService: UserService,
-      private router: Router, private modalService: BsModalService) { }
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService,
+    private userService: UserService,
+    private router: Router,
+    private modalService: BsModalService
+  ) {}
 
   ngOnInit() {
-    this.authService.currentPhotoUrl.subscribe(photoUrl => this.photoUrl = photoUrl);
-    this.userService.isUnread.subscribe(isUnRead => this.isUnRead = isUnRead);
+    this.authService.currentPhotoUrl.subscribe(
+      (photoUrl) => (this.photoUrl = photoUrl)
+    );
+    this.userService.isUnread.subscribe(
+      (isUnRead) => (this.isUnRead = isUnRead)
+    );
   }
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
@@ -40,33 +48,36 @@ export class NavComponent implements OnInit {
   cancelRegister(cancelFlag: boolean) {
     this.registerMode = cancelFlag;
     this.modalRef.hide();
-   }
+  }
   login() {
-    this.authService.login(this.model).subscribe(next => {
-      this.alertify.success('Logged in successfully');
-     this.userService.areThereUnReadMessages(this.authService.currentUser.id);
-      console.log(this.isUnRead);
-    }, error => {
-      this.alertify.error(error);
-    }, () => {
-      this.router.navigate(['/members']);
-    });
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        this.alertify.success("Logged in successfully");
+        this.userService.areThereUnReadMessages(
+          this.authService.currentUser.id
+        );
+        console.log(this.isUnRead);
+      },
+      (error) => {
+        this.alertify.error(error);
+      },
+      () => {
+        this.router.navigate(["/dashboard"]);
+      }
+    );
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     return !!token;
   }
 
-
-
   logout() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
     this.authService.decodedToken = null;
     this.authService.currentUser = null;
-    this.alertify.message('logged out');
-    this.router.navigate(['/home']);
+    this.alertify.message("logged out");
+    this.router.navigate(["/news"]);
   }
-
 }
